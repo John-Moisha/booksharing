5cd289778837fde47177e156927a45ef90e65424
John-Moisha <pkgames.dp@gmail.com>
2021-04-01 01:21:48 -0400

    add token generation to activation account

--- APP/accounts/views.py
+++ APP/accounts/views.py
@@ -7,7 +7,7 @@ from annoying.functions import get_object_or_None
 from accounts.forms import SighUpForm
 from accounts.models import User, ContactUs
 from accounts.tasks import send_contact_us_email
-
+from django.contrib.auth.tokens import default_token_generator
 
 class MyProfileView(LoginRequiredMixin, UpdateView):
     queryset = User.objects.all()
@@ -51,20 +51,6 @@ class SignUpView(CreateView):
     form_class = SighUpForm
     template_name = 'accounts/signup.html'
 
-# def signup2(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('index')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'accounts/signup2.html', {'form': form})
-
 
 class ActivateView(RedirectView):
     pattern_name = 'login'
@@ -72,26 +58,8 @@ class ActivateView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         username = kwargs.pop('username')
         user = get_object_or_None(User, username=username, is_active=False)
-        if user:
+        if user is not None and default_token_generator.check_token(user, token):
             user.is_active = True
             user.save(update_fields=('is_active', ))
             messages.add_message(self.request, messages.INFO, 'Your account is activated!')
         return super().get_redirect_url(*args, **kwargs)
-
-
-# class ActivateAccountView(View):
-#     def get(self, request, uidb64, token):
-#         try:
-#             uid = force_text(urlsafe_base64_decode(uidb64))
-#             user = User.objects.get(pk=uid)
-#         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-#             user = None
-#
-#         if user is not None and account_activation_token.check_token(user, token):
-#             user.profile.email_confirmed = True
-#             user.save()
-#             login(request, user)
-#             return redirect('index')
-#         else:
-#             # invalid link
-#             return render(request, 'registration/invalid.html')
